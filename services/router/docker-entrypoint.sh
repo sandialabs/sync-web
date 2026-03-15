f3bccbe29da6c40447bd3e0f7cc39b6f61aaf11d
Thien-Nam Dinh <thidinh@sandia.gov>
2026-03-15 08:56:30 +0000

    Render router upstream hosts from environment

--- services/router/docker-entrypoint.sh
+++ services/router/docker-entrypoint.sh
@@ -3,6 +3,52 @@ set -eu
 
 CERT_FILE="${TLS_CERT_FILE:-/etc/nginx/certs/tls.crt}"
 KEY_FILE="${TLS_KEY_FILE:-/etc/nginx/certs/tls.key}"
+JOURNAL_HOST="${ROUTER_JOURNAL_HOST:-journal}"
+GATEWAY_HOST="${ROUTER_GATEWAY_HOST:-gateway}"
+EXPLORER_HOST="${ROUTER_EXPLORER_HOST:-explorer}"
+WORKBENCH_HOST="${ROUTER_WORKBENCH_HOST:-workbench}"
+
+cat > /etc/nginx/includes/nginx.routes.inc <<EOF
+location /interface {
+    proxy_pass http://${JOURNAL_HOST}/interface;
+}
+
+location = /gateway {
+    return 301 /gateway/;
+}
+
+location /gateway/ {
+    proxy_pass http://${GATEWAY_HOST}/;
+}
+
+location /api/ {
+    proxy_pass http://${GATEWAY_HOST};
+}
+
+location = /docs {
+    proxy_pass http://${GATEWAY_HOST}/docs;
+}
+
+location = /api/v1/docs {
+    proxy_pass http://${GATEWAY_HOST}/api/v1/docs;
+}
+
+location = /healthz {
+    proxy_pass http://${GATEWAY_HOST}/healthz;
+}
+
+location = /readyz {
+    proxy_pass http://${GATEWAY_HOST}/readyz;
+}
+
+location /explorer {
+    proxy_pass http://${EXPLORER_HOST}/;
+}
+
+location /workbench {
+    proxy_pass http://${WORKBENCH_HOST}/;
+}
+EOF
 
 if [ -f "$CERT_FILE" ] \
     && [ -f "$KEY_FILE" ] \
